7a722986b45d13dffa615666d81e1b4123fcf508
kathiver <132450714+kathiver@users.noreply.github.com>
2025-02-09 17:42:40 +0100

    nytt prosjekt
    nytt prosjekt lagt til

---
+++ packtestigjen/packtestigjen/packtestigjenApp.swift
@@ -0,0 +1,17 @@
+//
+//  packtestigjenApp.swift
+//  packtestigjen
+//
+//  Created by Kathrine Iversen on 09/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct packtestigjenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ packtestigjen/packtestigjenTests/packtestigjenTests.swift
@@ -0,0 +1,17 @@
+//
+//  packtestigjenTests.swift
+//  packtestigjenTests
+//
+//  Created by Kathrine Iversen on 09/02/2025.
+//
+
+import Testing
+@testable import packtestigjen
+
+struct packtestigjenTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
